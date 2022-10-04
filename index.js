b2d007f3a2aba0af49a449352990c8e045d91ff2
Christopher Sloss <ttg1989_89@protonmail.com>
2022-10-04 17:46:46 -0400

    commit tree f76d7f30b5def01350c1f50132d44ae403886600

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Chris",
+    streetAddress: "2840 Somerton Drive",
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value) //Update employee non-destructively.
+{
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) //Update employee destructively.
+{
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key) // Delete employee non-destructively.
+{
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key) // Delete employee destructively.
+{
+    delete employee[key];
+    return employee;
+}
